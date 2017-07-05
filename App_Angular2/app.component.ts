3a2795e86b4faedb9ac59c7251ecabe9c075be27
Alencar Moreira Gonçalves Porto <alencarmoreira@snews.tv>
2017-07-05 13:03:46 -0300

    Alterações para primeiro funcionamento correto do angular 2.0.

---
+++ App_Angular2/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'minha-app',
+    template: '<h1>Minha primeira aplicação Angular 2.0</h1><br>Alencar Porto'
+})
+export class MeuPrimeiroComponente { }
---
+++ App_Angular2/app.module.ts
@@ -0,0 +1,10 @@
+﻿
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MeuPrimeiroComponente } from './app.component';
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [MeuPrimeiroComponente],
+    bootstrap: [MeuPrimeiroComponente]
+})
+export class AppModule { }
